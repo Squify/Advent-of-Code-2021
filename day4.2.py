702d18885cd0afecddc60635dd05fe617fcdfb70
Pauline <40692004+Squify@users.noreply.github.com>
2021-12-05 18:05:27 +0100

    Day 4 - Complete

--- day4.2.py
+++ day4.2.py
@@ -1,73 +1,83 @@
-m = []
+import numpy as np
 
-with open('example.txt') as f:
+cards = []
+cards_truth = []
+
+
+def check_bingo(card_truth, line, col):
+    valid = True
+    for i in range(0, len(card_truth[line])):
+        if not card_truth[line][i]:
+            valid = False
+            break
+
+    if valid:
+        return True
+
+    for i in range(0, len(card_truth)):
+        if not card_truth[i][col]:
+            return False
+
+    return True
+
+
+def verify_number(bingo_number, card, card_truth):
+    for i in range(0, len(card)):
+        for j in range(0, len(card[i])):
+            card_number = card[i][j]
+            if card_number == int(bingo_number):
+                card_truth[i][j] = True
+                if check_bingo(card_truth, i, j):
+                    return True
+    return False
+
+
+def calcul_points(winner_card, winner_card_truth):
+    points = 0
+    for i in range(0, len(winner_card)):
+        for j in range(0, len(winner_card[i])):
+            if not winner_card_truth[i][j]:
+                points = points + winner_card[i][j]
+    return points
+
+
+def search_winner():
+    print(cards_truth)
+    winners = []
+    sum_of_unmarked = 0
+    points = 0
+    for bingo_number in bingo_numbers:
+        print("searching ", bingo_number)
+        for card_index in range(0, len(cards)):
+            card = cards[card_index]
+            card_truth = cards_truth[card_index]
+            if card_index not in winners and verify_number(bingo_number, card, card_truth):
+                winners.append(card_index)
+                sum_of_unmarked = calcul_points(card, card_truth)
+                points = sum_of_unmarked * int(bingo_number)
+        if len(cards) == len(winners):
+            break
+    print(winners[-1], "with sum", sum_of_unmarked, "and", points, "points")
+
+
+with open('input4.txt') as f:
     bingo_numbers = f.readline().strip().split(',')
     f.readline()
-    n = []
-    for index, line in enumerate(f):
+    card = []
+    card_truth = []
+    for line in f:
         if line != "\n":
             list1 = line.strip().split()
             map_object = map(int, list1)
-            list2 = list(map_object)
-            n.append(list2)
+            line = list(map_object)
+            card.append(line)
+            card_truth.append([False] * len(line))
+        else:
+            cards.append(card)
+            cards_truth.append(card_truth)
+            card = []
+            card_truth = []
+    cards.append(card)  # la derniere ligne n'est pas un \n
+    cards_truth.append(card_truth)
 
-        if line == "\n":
-            m.append(n)
-            n = []
-    m.append(n)
-
-print(m)
-
-copy_okaou = m.copy()
-matrice_index = 0
-for number in bingo_numbers:
-    if matrice_index < len(m):
-        for line in m[matrice_index]:
-            print(int(number) in line)
-        matrice_index += 1
-    matrice_index = 0
-
-
-m2 = []
-matrice_index = 0
-if matrice_index < len(m):
-    for index, line in enumerate(m[matrice_index]):
-        n = []
-
-
-        m2[matrice_index]
-
-
-
-
-# 1- boucler sur les nombres du bingo_numbers
-# 2- regarder dans chaque matrice si ya le nombre
-# 3- si ya le nombre on mets un X
-# 4- on vérifie si ya une ligne complète qqpart
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+search_winner()
